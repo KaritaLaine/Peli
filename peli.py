61ddf6a81a7d3d58429f2a32318f08a8788e2392
KaritaLaine <44746@edu.raseko.fi>
2021-12-13 10:41:26 +0200

    Perustoiminnot toiminnassa
    viimeviikon + tämän aamun työt toimintakunnossa

---
+++ peli.py
@@ -0,0 +1,101 @@
+import pygame
+from pygame.constants import K_LEFT
+taustavari = (180, 240, 240)
+
+def main():
+    peli = Peli()
+    peli.aja()
+ 
+ 
+class Peli:
+    def __init__(self):
+        self.ajossa = True
+        self.naytto = None
+        self.leveys = 800
+        self.korkeus = 600
+        self.nayton_koko = (self.leveys, self.korkeus)
+ 
+    def aja(self):
+        self.alustus()
+        while self.ajossa:
+            for event in pygame.event.get():
+                self.tapahtuma(event)
+            self.pelilogiikka()
+            self.renderointi()
+        self.lopetus()
+ 
+    def alustus(self):
+        pygame.init()
+        self.kello = pygame.time.Clock()
+        self.naytto = pygame.display.set_mode(
+            self.nayton_koko, pygame.HWSURFACE | pygame.DOUBLEBUF)
+        self.kuva_iso = pygame.image.load("Pogey.png")
+        self.kuva_pieni = pygame.transform.rotozoom(self.kuva_iso, 0, 0.25)
+        self.kulma = 0
+        self.pyorimisvauhti = 0
+        self.sijainti = (400, 300)
+        self.nappi_pohjassa = False
+        self.voima = 0
+        self.voimanlisays = False
+        self.laukaisu = False
+ 
+    def tapahtuma(self, event):
+        if event.type == pygame.QUIT: 
+            self.ajossa = False
+        # Hiiren tapahtumat ------------------------------------
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            self.nappi_pohjassa = True
+        elif event.type == pygame.MOUSEBUTTONUP:
+            self.nappi_pohjassa = False
+        # Näppäimen painaminen alas ----------------------------
+        elif event.type == pygame.KEYDOWN:
+            if event.key == K_LEFT:
+                self.pyorimisvauhti = 3
+            elif event.key == pygame.K_RIGHT:
+                self.pyorimisvauhti = -3
+            elif event.key == pygame.K_SPACE:
+                self.voimanlisays = True
+        # Näppäimen nosto ylös ---------------------------------
+        elif event.type == pygame.KEYUP:
+            if event.key in (pygame.K_LEFT, pygame.K_RIGHT):
+                self.pyorimisvauhti = 0
+            elif event.key == pygame.K_SPACE:
+                self.voimanlisays = False
+                self.laukaisu = True
+
+
+ 
+    def pelilogiikka(self):
+        if self.nappi_pohjassa:
+            self.sijainti = pygame.mouse.get_pos()
+
+        if self.pyorimisvauhti != 0:
+            self.kulma = (self.kulma + self.pyorimisvauhti) % 360
+
+        if self.voimanlisays:
+            self.voima += 1
+            if self.voima > 100:
+                self.voima = 100
+        
+        if self.laukaisu:
+            print(f"Pam! {self.voima}")
+            self.voima = 0
+            self.laukaisu = False
+
+ 
+    def renderointi(self):
+        self.naytto.fill(taustavari) # (Red, Green, Blue)
+        kuva = pygame.transform.rotozoom(self.kuva_pieni, self.kulma, 1)
+        laatikko = kuva.get_rect(center=(self.sijainti))
+        self.naytto.blit(kuva, laatikko.topleft)
+        pygame.display.flip()
+        self.kello.tick(60) # 60 FPS (Frames Per Second)
+        # if self.kulma % 100 == 0:
+            # print(self.kello.get_fps())
+ 
+    def lopetus(self):
+        pygame.quit()
+ 
+ 
+if __name__ == "__main__" :
+    main()
